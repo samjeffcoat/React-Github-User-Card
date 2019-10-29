6c03ead677723e468fff64702068ac4c68b1d47d
Sam <Jeffcoat>
2019-10-29 14:03:49 -0500

    Sets up class component, so I can fetch the api data and see what the response is

--- github-user/src/App.js
+++ github-user/src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class  App extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      users:[]
+    }
+  }
+  render(){
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +28,6 @@ function App() {
       </header>
     </div>
   );
-}
+}}
 
 export default App;
